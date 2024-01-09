a683929ce9ccadab822c2a538775e96789c4acc0
ChloeL-E <chloe_c123@hotmail.co.uk>
2024-01-09 13:04:04 +0000

    new js code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-$(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
+$(document).ready(function () {
+    $("#stream1_btn").click(function () {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream1").addClass('highlight_stream');
-   });
-   $("#stream2_btn").on("click", function() {
+    });
+    $("#stream2_btn").on("click", function () {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream2").addClass('highlight_stream');
-   });
-   $("#stream3_btn").on("click", function() {
+    });
+    $("#stream3_btn").on("click", function () {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
-   });
+    });
+    $("p").on("click", function () {
+        $(".card_para").css("color", "red");
+    });
+    $("h2").mouseenter(function () {
+        $(".card_head").css("background-color", "lightblue");
+    });
+    $("#bigger").mouseenter(function () {
+        $("h2").css("font-size", "2em");
+    });
+    $("a").mouseenter(function () {
+        $("body").css("background-color", "black");
+    });
+    $("a").mouseleave(function () {
+        $("body").css("background-color", "grey");
+    });
 }); 
